67f36fa3df4390eda0812ee59020024e36851695
alexander3005 <alex30_93@mail.ru>
2015-10-25 21:59:01 +0300

    Second

---
+++ HibernateAplication/src/test/java/app/student/Dao/StudentDaoImplTest.java
@@ -0,0 +1,105 @@
+package app.student.Dao;
+
+
+
+import app.student.Model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.Transaction;
+
+
+
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+
+
+/**
+ * Created by Acer on 16.10.2015.
+ */
+public class StudentDaoImplTest {
+
+private Session session;
+  private SessionFactory sessionFactory;
+    private SessionFactory createSessionFactory(){
+        sessionFactory=  new Configuration().configure().buildSessionFactory();
+        return sessionFactory;
+    }
+  
+          public void before() {
+        sessionFactory = createSessionFactory();
+
+        session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+    }
+
+   
+    public void testAddStudent() throws Exception {
+sessionFactory =createSessionFactory();
+        session=sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Student> students =session.createQuery("from Student").list();
+        Student student=new Student();
+        student.setFirstName("Sasha");
+        student.setSecondName("Lix");
+         session.persist(student);
+        List<Student> students1 =session.createQuery("from Student").list();
+        assertTrue(students.size()< students1.size());
+
+    }
+
+  
+    public void testListStudent() throws Exception {
+sessionFactory =createSessionFactory();
+        session=sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Student student = new Student();
+        student.setFirstName("sss");
+        student.setSecondName("sssss");
+        session.persist(student);
+        List<Student> students=session.createQuery("from Student ").list();
+       assertTrue(students.size()>0);
+    }
+
+   
+    public void testUpdate() throws Exception {
+sessionFactory =createSessionFactory();
+        session=sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+Student student =new Student();
+        student.setId(95);
+        student.setFirstName("Alex");
+        student.setSecondName("M");
+        session.update(student);
+        Student student1=new Student();
+        student1=(Student)session.get(Student.class,new Integer(student.getId()));
+        assertEquals(student.getId(),student1.getId());
+        assertEquals(student.getFirstName(),student1.getFirstName());
+        assertEquals(student.getSecondName(),student1.getSecondName());
+
+    }
+
+  
+    public void testDelete() throws Exception {
+sessionFactory =createSessionFactory();
+        session=sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Student> students =session.createQuery("from Student").list();
+        Student student =new Student();
+        student.setFirstName("Alex");
+        student.setSecondName("M");
+        session.persist(student);
+        session.delete(student);
+        List<Student> students1 =session.createQuery("from Student").list();
+        assertEquals(students,students1);
+
+    }
+}
